ea485d5b4ab7f90e8b28f1040ab7def01212ea7f
Tomas Valent <equivalent@eq8.eu>
2015-03-24 10:16:02 +0000

    more sence to test

--- test/password_encrypter_test.rb
+++ test/password_encrypter_test.rb
@@ -3,11 +3,14 @@ require 'minitest/autorun'
 require 'bcrypt'
 
 describe described_class = DevisePasswordEncrypter::PasswordEncrypter do
+  let(:subject) { described_class.new() }
+
   describe '#call' do
-    let(:encrypted_password) { described_class.new().call(password)  }
-    let('password') { 'abcd123' }
+    let(:encrypted_password) { subject.call(password)  }
+    let(:password) { 'abcd123' }
 
     it 'should generate encrypted password' do
+      # bcrypted passowrds are 60 char in length
       encrypted_password.must_be_kind_of String
       encrypted_password.length.must_equal 60
     end
